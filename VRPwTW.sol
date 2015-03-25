5dad92e85f6c917b8e6c4f7279935c4070446b5a
adanilova <evamonara@gmail.com>
2015-03-25 22:33:29 +0100

    V1
    Version 1
    Without constraints ## 12-20

--- VRPwTW.sol
+++ VRPwTW.sol
@@ -1,37 +1,37 @@
 Travel_Cost = 618.2
 
 X :=
-Aberdeen  Banchory    5   1
-Aberdeen  Fyvie       1   1
-Aberdeen  Newmill     3   1
+Aberdeen  Banchory    1   1
+Aberdeen  Fyvie       3   1
+Aberdeen  Newmill     4   1
 Aberdeen  Peterhead   2   1
-Aberdeen  Towie       4   1
-Banchory  AberdeenEND 5   1
-Clova     Dufftown    4   1
-Cornhill  Newbyth     1   1
-Dufftown  Huntly      4   1
-Fyvie     Turiff      1   1
-Huntly    AberdeenEND 4   1
-Newbyth   AberdeenEND 1   1
-Newmill   AberdeenEND 3   1
+Aberdeen  Towie       5   1
+Banchory  AberdeenEND 1   1
+Clova     Dufftown    5   1
+Cornhill  Newbyth     3   1
+Dufftown  Huntly      5   1
+Fyvie     Turiff      3   1
+Huntly    AberdeenEND 5   1
+Newbyth   AberdeenEND 3   1
+Newmill   AberdeenEND 4   1
 Peterhead Strichen    2   1
 Strichen  AberdeenEND 2   1
-Towie     Clova       4   1
-Turiff    Cornhill    1   1
+Towie     Clova       5   1
+Turiff    Cornhill    3   1
 ;
 
 Visit :=
-Banchory  5   1
-Clova     4   1
-Cornhill  1   1
-Dufftown  4   1
-Fyvie     1   1
-Huntly    4   1
-Newbyth   1   1
-Newmill   3   1
+Banchory  1   1
+Clova     5   1
+Cornhill  3   1
+Dufftown  5   1
+Fyvie     3   1
+Huntly    5   1
+Newbyth   3   1
+Newmill   4   1
 Peterhead 2   1
 Strichen  2   1
-Towie     4   1
-Turiff    1   1
+Towie     5   1
+Turiff    3   1
 ;
 
